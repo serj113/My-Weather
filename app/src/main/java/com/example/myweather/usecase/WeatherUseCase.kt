01c0be4391641132c8255f6b8751b001929da7e4
Setia Simaremare <setia_simaremare@yahoo.com>
2019-12-18 11:03:59 +0700

    added test for weather use case

--- app/src/main/java/com/example/myweather/usecase/WeatherUseCase.kt
+++ app/src/main/java/com/example/myweather/usecase/WeatherUseCase.kt
@@ -1,4 +1,7 @@
 package com.example.myweather.usecase
 
+import com.example.myweather.domain.Weather
+
 interface WeatherUseCase {
+    fun getWeathers(): List<Weather>
 }
--- app/src/main/java/com/example/myweather/usecase/WeatherUseCaseImpl.kt
+++ app/src/main/java/com/example/myweather/usecase/WeatherUseCaseImpl.kt
@@ -1,4 +1,10 @@
 package com.example.myweather.usecase
 
-class WeatherUseCaseImpl {
+import com.example.myweather.domain.Weather
+import com.example.myweather.repository.WeatherRepository
+
+class WeatherUseCaseImpl(private val weatherRepository: WeatherRepository): WeatherUseCase {
+    override fun getWeathers(): List<Weather> {
+        return weatherRepository.getWeathers()
+    }
 }
