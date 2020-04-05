ac3f08240eecd8101dde14a7ecb7a123a64a8f4d
Setia <setia_simaremare@yahoo.com>
2020-04-05 22:42:14 +0700

    add fusedLocation

--- app/src/main/java/com/example/myweather/ui/weatherlist/WeatherListFragment.kt
+++ app/src/main/java/com/example/myweather/ui/weatherlist/WeatherListFragment.kt
@@ -1,26 +1,48 @@
 package com.example.myweather.ui.weatherlist
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.os.Looper
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
+import android.widget.Toast
+import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myweather.databinding.FragmentWeatherListBinding
-import com.example.myweather.utils.ViewModelFactory
 import com.example.myweather.ui.viewmodel.WeatherListViewModel
-
+import com.example.myweather.utils.ViewModelFactory
+import com.google.android.gms.location.*
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
+
 
 class WeatherListFragment : Fragment() {
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
 
     private lateinit var adapter: MyWeatherRecyclerViewAdapter
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult?) {
+            super.onLocationResult(p0)
+            p0?.let {
+                Toast.makeText(requireContext(), p0.lastLocation.latitude.toString(), Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private var PERMISSION_ID = 44
 
     private val viewModel by lazy {
         ViewModelProviders.of(requireActivity(), viewModelFactory).get(WeatherListViewModel::class.java)
@@ -28,7 +50,15 @@ class WeatherListFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         AndroidSupportInjection.inject(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+//        if (ActivityCompat.checkSelfPermission(
+//                requireContext(),
+//                Manifest.permission.ACCESS_COARSE_LOCATION
+//            ) == PackageManager.PERMISSION_GRANTED) {
+//        }
+//        getLastLocation()
     }
 
     override fun onCreateView(
@@ -50,5 +80,87 @@ class WeatherListFragment : Fragment() {
         })
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getLastLocation()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation()
+            }
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        return checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startLocationPermissionRequest() {
+        requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), PERMISSION_ID)
+    }
+
+    private fun requestPermissions() {
+        requestPermissions(
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            PERMISSION_ID
+        )
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager = getSystemService(requireContext(), LocationManager::class.java)
+        locationManager?.let {
+            return it.isProviderEnabled(LocationManager.GPS_PROVIDER) || it.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+            )
+        }
+        return false
+    }
+
+    private fun getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                fusedLocationClient.lastLocation.addOnCompleteListener { task ->
+                    val location: Location? = task.result
+                    if (location == null) {
+                        requestNewLocationData()
+                    } else {
+                        Toast.makeText(requireContext(), location.latitude.toString(), Toast.LENGTH_LONG).show()
+//                        latTextView.setText(location.getLatitude().toString() + "")
+//                        lonTextView.setText(location.getLongitude().toString() + "")
+                    }
+                }
+            } else {
+                Toast.makeText(requireContext(), "Turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+//            requestPermissions()
+            startLocationPermissionRequest()
+        }
+    }
+
+    fun requestNewLocationData() {
+        val locationRequest = LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 1
+            fastestInterval = 1
+            numUpdates = 1
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
     }
 }
